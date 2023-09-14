3a48ccf9a132da5e906f3efbe5f7e29407650bc0
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 11:01:35 +0200

    Update Mograph.js

--- my-cv/src/rutas/Mograph.js
+++ my-cv/src/rutas/Mograph.js
@@ -1,5 +1,5 @@
 export const mograph = [{
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Mograph/Mograph.mp4",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Mograph/Mograph.mp4?raw=true",
     width: 16,
     height: 9,
 }]
