ee469861a6b78aa6af60d5b4cca27b2d2f37807a
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 10:29:49 +0200

    Update Ricky.js

--- my-cv/src/rutas/Ricky.js
+++ my-cv/src/rutas/Ricky.js
@@ -1,16 +1,16 @@
 export const ricky = [
     {
-      src: "/media/Ricky/P05_JoseAntonioSosa_R01_R1.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Ricky/P05_JoseAntonioSosa_R01_R1.png",
       width: 16,
       height: 9,
     },
     {
-      src: "/media/Ricky/P05_JoseAntonioSosa_R01_R02.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Ricky/P05_JoseAntonioSosa_R01_R02.png",
       width: 16,
       height: 9,
     },
     {
-      src: "/media/Ricky/P05_JoseAntonioSosa_R01_R03.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Ricky/P05_JoseAntonioSosa_R01_R03.png",
       width: 16,
       height: 9,
     },
