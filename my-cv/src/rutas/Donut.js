175d5458c2f6408a457d947f3029f409fa41270a
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 11:00:28 +0200

    Update Donut.js

--- my-cv/src/rutas/Donut.js
+++ my-cv/src/rutas/Donut.js
@@ -1,5 +1,5 @@
 export const donut = [{
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Donut/Donut.mp4",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Donut/Donut.mp4?raw=true",
     width: 9,
     height: 21,
 }]
