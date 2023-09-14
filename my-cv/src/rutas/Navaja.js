de1a0241ed57f23587dfcf92cc8c00f308cfb80f
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 10:29:17 +0200

    Update Navaja.js

--- my-cv/src/rutas/Navaja.js
+++ my-cv/src/rutas/Navaja.js
@@ -1,31 +1,31 @@
 export const navaja = [
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_1.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_1.png",
     width: 16,
     height: 9,
   },
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_2.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_2.png",
     width: 16,
     height: 9,
   },
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_3.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_3.png",
     width: 16,
     height: 9,
   },
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_4.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_4.png",
     width: 16,
     height: 9,
   },
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_5.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_5.png",
     width: 16,
     height: 9,
   },
   {
-    src: "/media/Navaja/P02_JoseAntonioSosa_R01_6.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_6.png",
     width: 16,
     height: 9,
   },
