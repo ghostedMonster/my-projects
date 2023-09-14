3da6e087b15dcc81a26c67a93456d861e3d7b6fd
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 11:02:06 +0200

    Update Navaja.js

--- my-cv/src/rutas/Navaja.js
+++ my-cv/src/rutas/Navaja.js
@@ -1,31 +1,31 @@
 export const navaja = [
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_1.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_1.png?raw=true",
     width: 16,
     height: 9,
   },
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_2.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_2.png?raw=true",
     width: 16,
     height: 9,
   },
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_3.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_3.png?raw=true",
     width: 16,
     height: 9,
   },
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_4.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_4.png?raw=true",
     width: 16,
     height: 9,
   },
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_5.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_5.png?raw=true",
     width: 16,
     height: 9,
   },
   {
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_6.png",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Navaja/P02_JoseAntonioSosa_R01_6.png?raw=true",
     width: 16,
     height: 9,
   },
