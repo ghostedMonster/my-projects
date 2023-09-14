61e456a9833e9c0941e60d44375c6c0d0611f4f6
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 10:59:56 +0200

    Update Amongus.js

--- my-cv/src/rutas/Amongus.js
+++ my-cv/src/rutas/Amongus.js
@@ -1,21 +1,21 @@
 export const amongus = [
     {
-      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R01.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R01.png?raw=true",
       width: 16,
       height: 9,
     },
     {
-      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R02.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R02.png?raw=true",
       width: 16,
       height: 9,
     },
     {
-      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R03.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R03.png?raw=true",
       width: 16,
       height: 9,
     },
     {
-      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R04.png",
+      src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Amongus/P04_JoseAntonioSosa_R04.png?raw=true",
       width: 9,
       height: 16,
     },
