4a1c3dcd7a604a2074f9a770bf2c9d699097324e
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 11:00:13 +0200

    Update Auriculares.js

--- my-cv/src/rutas/Auriculares.js
+++ my-cv/src/rutas/Auriculares.js
@@ -1,5 +1,5 @@
 export const auriculares = [{
-    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Auriculares/Auriculares.mp4",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Auriculares/Auriculares.mp4?raw=true",
     width: 16,
     height: 9,
 }]
