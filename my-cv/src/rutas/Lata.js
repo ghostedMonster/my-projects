e2eef65ae179cb3597aa6f7aab77e0a255e5b3b4
Jose Antonio Sosa Cifuentes <jose-antonio-1110@hotmail.com>
2023-09-14 10:28:16 +0200

    Update Lata.js

--- my-cv/src/rutas/Lata.js
+++ my-cv/src/rutas/Lata.js
@@ -1,5 +1,5 @@
 export const lata = [{
-    src: "/media/Lata/lata.mp4",
+    src: "https://github.com/ghostedMonster/my-projects/blob/main/my-cv/public/media/Lata/lata.mp4",
     width: 9,
     height: 16,
 }]
